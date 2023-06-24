0eb1a8687673269ce16ced721d5ce7c50fe39534
Anawach Anantachoke <achinhchin@Thakoons-MacBook-Air.local>
2023-06-24 21:56:25 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/achinhchin/wataslim/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.achinhchin.wataslim
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,268 @@
+import 'package:flutter/material.dart';
+import 'package:blur/blur.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 255, 247, 93)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+  double blurAmount = 0.0;
+  final List<String> watalist = [
+    'รู้มั้ยว่าพ่อทำอะไรบ้าง',
+    'พวกชังชาติ',
+    'พวกทำลายชาติ',
+    'ส้มเน่า',
+    'จั๊กแร้พิธาหอมไหม',
+    'รักชาติ ศาสน์ กษัตริย์',
+    'ไม่สำนึกในบุญคุณ',
+    'หนักแผ่นดิน',
+    'เนรคุณแผ่นดิน',
+    'เราคือผู้อยู่อาศัย ไม่ใช่เจ้าของบ้าน',
+    'ล้มเจ้า',
+    'เผาไทย',
+    'คณะร่าน',
+    'เห่อหมอยคลั่งชาติฝรั่ง',
+    'มีทุกอย่างที่ดีเพราะใคร ฉันจะไม่ลืม',
+    'พวกเผาบ้านเผาเมือง',
+    'จำนำข้าว',
+    'เป็นคนปลอมตัวมาสร้างสถานการณ์ค่ะ คนเสื้อเหลืองไม่มีใครทำแบบนั้น',
+    'ศูนย์รวมจิตใจของชาติ',
+    'ตอบแทนบุญคุณแผ่นดิน',
+    'สวะส้ม',
+    'ผมก็ว่าเนชั่นเป็นกลางสุดแล้วในการเสนอข่าว ไม่ได้อวยใดๆพูดตามเนื้อผ้าครับ',
+    'สร้างแต่ปัญหาให้ลุงตู่ แผนตื้นๆยังไปติดกับดัก',
+    'ไม่เอาต่างชาติเป็นนาย',
+    'เหยียบย่ำหัวใจคนไทย',
+    'มีเวลาว่างก็เอาไปเรียนนะลูก',
+    '่ขี้ไอ้ทอน',
+    'ลุงมาจากการเลือกตั้ง',
+    'เชียร์ลุง',
+    'ที่มีแผ่นดินอยู่ทุกวันนี้เพราะใคร',
+    'ม๊อบสวะ',
+    'ทำประโยชน์อะไรให้ชาติบ้านเมืองบ้าง',
+    'ลุงตู่เป็นคนดี',
+    'ลุงตู่อยู่บ้านเมืองสงบ',
+    'บ้านเมืองสงบ จบที่ลุงตู่',
+    'รัฐประหารโดยสันติวิธี',
+    'เก่งมากก็ไปเป็นนายกฯเองสิ',
+    'แค่รถฉีดน้ำจะไปกลัวทำไม เขาทำตามหลักสากล',
+    'ท่านทรงงานหนัก',
+    'โดนแค่ฉีดน้ำทำเป็นบ่น ตอนนั้น กปปส โดนแก๊สน้ำตานะ',
+    'หัวก้าวหน้า ปัญญาล้าหลัง',
+    'อยากได้เสรีภาพมากเกินไป',
+    'วันๆ เอาแต่เรียกร้องเสรีภาพ ถึงไม่รู้ไงว่าท่านทำอะไรบ้าง',
+    'หัดศึกษาประวัติศาสตร์บ้างนะ',
+    'ถอยกันคนละก้าว',
+    'เจ้าจะทำอะไรก็เรื่องของเค้า',
+    'จ่ายภาษีหรือเปล่า',
+    'เด็กๆเอาแต่เล่นโซเชียล fake news ทั้งนั้น',
+    'รัฐบาลมีผลงานเยอะแยะ แค่ข่าวไม่ออกเท่านั้นแหละ',
+    'คิดต่างได้ แต่ต้องมีสถาบัน',
+    'รักประยุทธ์ ก็ยังดีกว่าโดนล้างสมอง',
+    'อยู่อย่างจงรัก ตายอย่างภักดี ปกป้องสถาบัน',
+    'ไปฟังคนไม่จบ ม.6 มันพูดทำไม',
+    'ขยันอ่านหนังสือสอบให้เหมือนอ่านเบิกเนตรหรือยัง',
+    'คุณภาพชีวิตจะดีขึ้น ถ้าคนเป็นคนดี ถ้าตัวเราดี',
+    'แล้วที่หลานทำไม่เรียกว่าคุกคามสถาบันหรือ',
+    'เป็น IO ดีกว่าเป็นควายให้ไอ้แม้วไอ้ทอนจูงจมูก',
+    'เยาวชนปลดแอ๊ก',
+    'ประชาธิปไตยแดกได้เหรอ',
+    'ควรอยู่อย่างพอเพียงนะ',
+    'ไม่เอาต่างชาติเป็นนาย',
+    'เหยียบย่ำหัวใจคนไทย',
+    'ชู 3 นิ้ว กูเอามึงตายเลย',
+    'ไม่ภูมิใจเหรอ ที่ได้ตอบแทนคุณแผ่นดิน',
+    'ให้ใครมาด่าพ่อแม่คุณไม่ผิดเอาไหม',
+    'แม้เหลือคนเดียวทั้งโรง เราก็จะยืน',
+    'ห้ามวิจารณ์ 112',
+    'อีเด็กพวกนี้มันจะไปรู้อะไร',
+    'พวกสามกีบ',
+    'ไม่รักลุงตู่ = ไม่รักชาติ',
+    'เดี๋ยวกูจัด 112 ให้',
+    'เกิดมามี 32 ทำไมโตมามี 3 กีบ',
+    'เขาอุตส่าห์ให้อยู่ในแผ่นดินของเขา',
+    'เนี่ย ถ้าไม่มีคสช.นะพวกนักการเมืองคงโกงกินประเทศไปมากกว่านี้ เห็นมั้ย ลุงตู่เข้ามาทำให้ประเทศเราไม่มีการคอร์รัปชั่น',
+    'ถ้าพ่อมองลงมาคงร้องไห้',
+    'อะไรๆก็ว่าเเต่คนรุ่นหลังคงจะเจริญนะอีเด็กพวกนี้',
+    'ไม่ใช่สลิ่มนะ แต่...',
+    'ไม่ใช่ติ่งลุงตู่ แต่...',
+    'เราคนไทยเหมือนกัน',
+    'อีช่อ',
+    'อีฉ้อ',
+    'ชังชาติ',
+    'ขายชาติ',
+    'ไม่สำนึกในบุญคุณ',
+    'หนักแผ่นดิน',
+    'เนรคุณแผ่นดินอีช่อ',
+    'อีฉ้อ',
+    'ชังชาติ',
+    'ขายชาติ',
+    'ไม่สำนึกในบุญคุณ',
+    'หนักแผ่นดิน',
+    'เนรคุณแผ่นดิน',
+    'ข่าวเนชั่นนำเสนอเป็นกลางสุดแล้ว',
+    'ใครไม่ยืนในโรงหนังกูเอาตาย',
+    'ขามึงขาดหรอยืนในโรงหนังไม่ได้',
+    'อีสามกีีบปัญญาอ่อน',
+  ];
+  String wataNow = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 10, end: 0).animate(_animationController);
+    randWata();
+  }
+
+  void randWata() {
+    Random random = Random();
+    wataNow = watalist[random.nextInt(watalist.length)];
+    _animationController.reset();
+    _animationController.forward();
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 20),
+            padding: const EdgeInsets.symmetric(
+              vertical: 5,
+              horizontal: 7.5,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.yellow[600],
+              borderRadius: BorderRadius.circular(5),
+              boxShadow: const [
+                BoxShadow(
+                  color: Color(0x40000000),
+                  offset: Offset(2, 2),
+                  blurRadius: 3,
+                ),
+              ],
+            ),
+            child: const Text(
+              'Cr.narze',
+              style: TextStyle(fontSize: 15),
+            ),
+          )
+        ],
+        backgroundColor: const Color.fromARGB(255, 249, 255, 87),
+        title: const Text(
+          "วาทะสลิ่มสุดเจ๋ง",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/img/thai-flag.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 13, horizontal: 23),
+            decoration: BoxDecoration(
+              color: Colors.yellow[300],
+              borderRadius: const BorderRadius.all(Radius.circular(15)),
+              boxShadow: const [
+                BoxShadow(
+                  offset: Offset(4, 4),
+                  color: Color(0x80000000),
+                  blurRadius: 10,
+                )
+              ],
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  'วาทะสลิ่ม: ',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: AnimatedBuilder(
+                    animation: _animation,
+                    builder: (context, child) => Blur(
+                      colorOpacity: 0,
+                      blur: _animation.value,
+                      blurColor: Colors.transparent,
+                      child: Text(
+                        '"$wataNow"',
+                        style: TextStyle(
+                          fontSize: 22.5,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: randWata,
+        tooltip: 'Generate',
+        label: Text(
+          "Generate วาทะสลิ่ม",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 17,
+            color: Theme.of(context).primaryColor,
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(255, 249, 255, 87),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
